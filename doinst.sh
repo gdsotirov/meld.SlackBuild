34c962fd6fa12b2bb92bfdfa2172d2331b051a3e
Georgi D. Sotirov <gdsotirov@dir.bg>
2016-07-21 19:08:59 +0000

    Check executable, add gtk-update-icon-cache and glib-compile-schemas

--- doinst.sh
+++ doinst.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
 # Update MIME database
-/usr/bin/update-mime-database /usr/share/mime
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database /usr/share/mime
+fi
+
 # Update Dekstop database
-/usr/bin/update-desktop-database /usr/share/applications
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database /usr/share/applications
+fi
+
+# Update icon cache
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+  /usr/bin/gtk-update-icon-cache /usr/share/icons/hicolor
+fi
+
+# Compile Glib schemas
+if [ -x /usr/bin/glib-compile-schemas ]; then
+  /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas
+fi
 
